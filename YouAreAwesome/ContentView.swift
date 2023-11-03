dfef817c61fedc1bdbda9cdfa975d364dd69ba34
Leo Barnett <leo.e.barnett@gmail.com>
2023-11-03 15:04:18 +0200

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Leo  on 03.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Leo  on 03.11.23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
