52c900f85afe2487f49287993048a93547c40591
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-06 21:57:56 +0200

    Version 4.0

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,46 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the genius bar needs help, they call you."
+    @State private var messageString = "Namaste"
     @State private var messageBool = true
     
     var body: some View {
+        
+        VStack {
             
-            VStack {
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.red)
-                    .italic()
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                   // .border(.orange, width: 1)
-                    .padding()
-                
-                Spacer()
-                
-                HStack {
-                    Button("Awesome"){
-                        messageString = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "You Are Great!"
-                        
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-               // .border(.purple, width: 5)
+            Spacer()
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.multicolor)
                 .padding()
+                .background(Color(hue: 0.51, saturation: 0.229, brightness: 0.88))
+                .cornerRadius(30)
+                .shadow(radius: 30, x: 20, y: 20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.teal, lineWidth: 1)
+                )
+                .padding()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.pink)
+                .italic()
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome"){
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                    
+                }
+                .buttonStyle(.borderedProminent)
             }
+            .padding()
+        }
     }
 }
 
