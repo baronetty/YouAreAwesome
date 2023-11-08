bb8161193a8e56bf9c6f12d0c33e77d03fb0b928
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-08 21:25:59 +0200

    Version 7.0

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,10 +11,18 @@ struct ContentView: View {
     
     @State private var messageString = ""
     @State private var messageBool = true
+    @State private var imageName = ""
     
     var body: some View {
         
         VStack {
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
             
             Spacer()
             
@@ -34,10 +42,21 @@ struct ContentView: View {
             
             Button("Show Message"){
                 
+                let imageName0 = "image0"
+                let imageName1 = "image1"
                 let message1 = "You Are Awesome!"
                 let message2 = "You Are Great!"
                 
-                messageString == message1 ? (messageString = message2) : (messageString = message1)
+                //                if messageString == message1 {
+                //                    messageString = message2
+                //                    imageName = imageName1
+                //                } else {
+                //                    messageString = message1
+                //                    imageName = imageName0
+                //                }
+                
+                messageString = (messageString == message1 ? message2 : message1)
+                imageName = (imageName == imageName0 ? imageName1 : imageName0)
             }
             .buttonStyle(.borderedProminent)
             .padding()
